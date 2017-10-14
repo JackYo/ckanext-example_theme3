4ff6164eb95ce05b36875c7c770802310cb16055
JackYo <frpsjack@gmail.com>
2017-10-14 15:47:00 +0800

    edit page also included

--- ckanext/example_theme3/plugin.py
+++ ckanext/example_theme3/plugin.py
@@ -4,10 +4,13 @@ import ckan.plugins.toolkit as toolkit
 
 class Example_Theme3Plugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-
+    plugins.implements(plugins.IResourceView)
     # IConfigurer
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'example_theme3')
+
+    def form_template(context, data_dict):
+        return u'templates/package/resource_edit.html'
